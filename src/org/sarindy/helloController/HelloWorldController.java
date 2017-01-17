21a225f54496110d6da55d6b45b2eafca6d585a0
sarindy <sarindyo@yahoo.com>
2017-01-17 20:58:14 +0700

    ok

--- src/org/sarindy/helloController/HelloWorldController.java
+++ src/org/sarindy/helloController/HelloWorldController.java
@@ -9,54 +9,53 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloWorldController {
-	
+
 	@RequestMapping("/")
-	public ModelAndView helloWorld(){
+	public ModelAndView helloWorld() {
 		ModelAndView model = new ModelAndView("myHelloPage");
-		model.addObject("myWelcomeMessage","បងសំណាងណាស់ដែលមានអូនក្នុងជាតិនេះ");
+		model.addObject("myWelcomeMessage", "បងសំណាងណាស់ដែលមានអូនក្នុងជាតិនេះ");
 		return model;
-		
+
 	}
-	
+
 	@RequestMapping("/hi")
-	public ModelAndView hiWorld(){
+	public ModelAndView hiWorld() {
 		ModelAndView model = new ModelAndView("myHelloPage");
-		model.addObject("myWelcomeMessage","Hi World...");
+		model.addObject("myWelcomeMessage", "Hi World...");
 		return model;
-		
+
 	}
-	
+
 	@RequestMapping("/bootstrap")
-	public ModelAndView getBootStrap(){
+	public ModelAndView getBootStrap() {
 		ModelAndView model = new ModelAndView("bootstrapTest");
-		model.addObject("htmlcode","<div class=\"container\">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's " + 
-		"standard dummy text ever since the 1500s, when an unknown printer took " +
-		"a galley of type and scrambled it to make a type specimen book. It has"+
-		"survived not only five centuries, but also the leap into electronic"+
-		"typesetting, remaining essentially unchanged. It was popularised in"+
-		"the 1960s with the release of Letraset sheets containing Lorem Ipsum"+
-		"passages, and more recently with desktop publishing software like"+
-		"Aldus PageMaker including versions of Lorem Ipsum.</div><p>");
+		model.addObject(
+				"htmlcode",
+				"<div class=\"container\">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's "
+						+ "standard dummy text ever since the 1500s, when an unknown printer took "
+						+ "a galley of type and scrambled it to make a type specimen book. It has"
+						+ "survived not only five centuries, but also the leap into electronic"
+						+ "typesetting, remaining essentially unchanged. It was popularised in"
+						+ "the 1960s with the release of Letraset sheets containing Lorem Ipsum"
+						+ "passages, and more recently with desktop publishing software like"
+						+ "Aldus PageMaker including versions of Lorem Ipsum.</div><p>");
 		return model;
-		
+
 	}
-	
+
 	@RequestMapping("/list")
-	public ModelAndView getList(){
-		
-		List<String> list=new ArrayList<String>();
-		
+	public ModelAndView getList() {
+
+		List<String> list = new ArrayList<String>();
+
 		list.add("1");
 		list.add("2");
 		list.add("3");
-		
-		
-		
-		ModelAndView model = new ModelAndView("myHelloPage");
-		model.addObject("myLists",list);
-		return model;
-		
-	}
 
+		ModelAndView model = new ModelAndView("myHelloPage");
+		model.addObject("myLists", list);
+		return model;
+
+	}
 
 }
